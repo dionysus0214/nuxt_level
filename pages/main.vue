bffe187eebb5ab1024645e54b21c9dc1759b268a
dionysus0214 <hiba0214@naver.com>
2023-01-04 23:08:13 +0900

    페이지 컴포넌트 생성 및 빌드

---
+++ pages/main.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtLogo />
+    <h1>메인 페이지</h1>
+  </div>
+</template>
+
+<script>
+import NuxtLogo from '@/components/NuxtLogo.vue';
+
+export default {
+  components: {
+    NuxtLogo,
+  }
+}
+</script>
